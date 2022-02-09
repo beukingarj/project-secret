a4890d4c5d2c0cf1e2393bc3f85f3937f0ff39ca
Jorn Beukinga <jorn.beukinga@sogeti.com>
2022-02-09 16:45:20 +0100

    9-2-1

--- script3.js
+++ script3.js
@@ -1,21 +1,62 @@
+const cirkel = document.querySelector('.cirkel');
 const totalSecondsEl = document.getElementById("totalSeconds");
-const newYears = "1 Jul 2022";
+const beginDate = new Date(2022, 1, 9, 14, 0, 0, 0);
+const endDate = new Date(2022, 1, 9, 15, 30, 0, 0);
+
+const sheight = 300;
+const swidth = 300;
+const sborder_radius = 300;
+const stop_css = -10;
+const sleft_css = 0;
+
+const fheight = 40;
+const fwidth = 40;
+const fborder_radius = 40;
+const ftop_css = 24;
+const fleft_css = -15;
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
     const currentDate = new Date();
+    const totalSeconds = (endDate - currentDate) / 1000;
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    if (totalSeconds >= 0)  {
+        totalSecondsEl.innerHTML = parseFloat(totalSeconds).toFixed(0);
+        const height = (currentDate-beginDate) / (endDate-beginDate) * (fheight-sheight) + sheight;
+        const width = (currentDate-beginDate) / (endDate-beginDate) * (fwidth-swidth) + swidth;
+        const border_radius = (currentDate-beginDate) / (endDate-beginDate) * (fborder_radius-sborder_radius) + sborder_radius;
+        const top_css = (currentDate-beginDate) / (endDate-beginDate) * (ftop_css-stop_css) + stop_css;
+        const left_css = (currentDate-beginDate) / (endDate-beginDate) * (fleft_css-sleft_css) + sleft_css;  
+        
+        cirkel.style.setProperty('--element-height', height + 'px')
+        cirkel.style.setProperty('--element-width', width + 'px')
+        cirkel.style.setProperty('--element-border-radius', border_radius + 'px')
+        cirkel.style.setProperty('--element-top', top_css + 'px')
+        cirkel.style.setProperty('--element-left', left_css + 'px')
+    } else {
+        totalSecondsEl.innerHTML = parseFloat(0).toFixed(0);
+        const height = fheight;
+        const width = fwidth;
+        const border_radius = fborder_radius;
+        const top_css = ftop_css;
+        const left_css = fleft_css; 
 
-    totalSecondsEl.innerHTML = parseFloat(totalSeconds).toFixed(0);
-
-    // cirkel.style.setProperty('--element-height', 10 + 'px')
-    // cirkel.style.setProperty('--element-width', 10 + 'px')
-
-    if (totalSeconds <= 0)  {
-        // window.location.href = "versie 2.html";
+        cirkel.style.setProperty('--element-height', height + 'px')
+        cirkel.style.setProperty('--element-width', width + 'px')
+        cirkel.style.setProperty('--element-border-radius', border_radius + 'px')
+        cirkel.style.setProperty('--element-top', top_css + 'px')
+        cirkel.style.setProperty('--element-left', left_css + 'px')
     }
-    
+
+    // cirkel.style.setProperty('--element-height', height + 'px')
+    // cirkel.style.setProperty('--element-width', width + 'px')
+    // cirkel.style.setProperty('--element-border-radius', border_radius + 'px')
+    // cirkel.style.setProperty('--element-top', top_css + 'px')
+    // cirkel.style.setProperty('--element-left', left_css + 'px')
+
+    // if (totalSeconds <= 0)  {
+    //     break
+    //     // window.location.href = "versie 2.html";
+    // }
     
 }
 
